d9e78a540c8fbf2c5e7b3a313c2f7df7f865cda9
Valentin Maerten <maerten.valentin@gmail.com>
2025-12-14 10:49:51 +0100

    ✨ feat: add settings UI
    Add FileNestingSettings for persistent state and FileNestingConfigurable
    for Settings > Appearance > File Nesting configuration panel.

---
+++ src/main/kotlin/com/github/valentin/filenesting/settings/FileNestingConfigurable.kt
@@ -0,0 +1,51 @@
+package com.github.valentin.filenesting.settings
+
+import com.intellij.openapi.options.BoundConfigurable
+import com.intellij.openapi.ui.DialogPanel
+import com.intellij.ui.dsl.builder.bindSelected
+import com.intellij.ui.dsl.builder.panel
+
+/**
+ * Settings UI for the File Nesting plugin.
+ *
+ * Provides a simple toggle to enable/disable file nesting in the Project View.
+ */
+class FileNestingConfigurable : BoundConfigurable("File Nesting") {
+
+    private val settings = FileNestingSettings.getInstance()
+
+    override fun createPanel(): DialogPanel = panel {
+        group("File Nesting") {
+            row {
+                checkBox("Enable file nesting in Project View")
+                    .bindSelected(settings.state::enabled)
+                    .comment("Group related configuration files under their parent files (e.g., .eslintrc under package.json)")
+            }
+        }
+
+        group("About") {
+            row {
+                text(
+                    """
+                    This plugin provides VS Code-like file nesting for JetBrains IDEs.
+                    <br><br>
+                    Patterns are based on <a href="https://github.com/antfu/vscode-file-nesting-config">antfu/vscode-file-nesting-config</a>.
+                    """.trimIndent()
+                )
+            }
+        }
+
+        group("Examples") {
+            row {
+                text(
+                    """
+                    <b>package.json</b> will group: .eslintrc, tsconfig.json, .prettierrc, lock files, etc.<br>
+                    <b>README</b> will group: LICENSE, CHANGELOG, CONTRIBUTING, etc.<br>
+                    <b>Dockerfile</b> will group: docker-compose files, .dockerignore, etc.<br>
+                    <b>*.ts</b> will group: corresponding .js, .d.ts, .map files
+                    """.trimIndent()
+                )
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/com/github/valentin/filenesting/settings/FileNestingSettings.kt
@@ -0,0 +1,47 @@
+package com.github.valentin.filenesting.settings
+
+import com.intellij.openapi.components.PersistentStateComponent
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.State
+import com.intellij.openapi.components.Storage
+
+/**
+ * Persistent settings for the File Nesting plugin.
+ *
+ * Settings are stored at the application level (IDE-wide, not per-project).
+ */
+@Service(Service.Level.APP)
+@State(
+    name = "FileNestingSettings",
+    storages = [Storage("fileNesting.xml")]
+)
+class FileNestingSettings : PersistentStateComponent<FileNestingSettings.State> {
+
+    /**
+     * The settings state that gets persisted.
+     */
+    data class State(
+        /**
+         * Whether file nesting is enabled.
+         */
+        var enabled: Boolean = true
+    )
+
+    private var myState = State()
+
+    override fun getState(): State = myState
+
+    override fun loadState(state: State) {
+        myState = state
+    }
+
+    companion object {
+        /**
+         * Get the settings instance.
+         */
+        fun getInstance(): FileNestingSettings {
+            return com.intellij.openapi.application.ApplicationManager.getApplication()
+                .getService(FileNestingSettings::class.java)
+        }
+    }
+}
